7209cc2d97a048c2d7f89e6a4d2abfc99108c625
Monsoir <monwingyeung@gmail.com>
2019-05-31 02:12:49 +0800

    refactor: 拆分代码

--- kvasir/Controllers/TopList/TopListCoordinator.swift
+++ kvasir/Controllers/TopList/TopListCoordinator.swift
@@ -27,6 +27,8 @@ class TopListCoordinator<Digest: RealmWordDigest>: ListQueryCoordinatorable {
     var updateHandler: (([IndexPath], [IndexPath], [IndexPath]) -> Void)?
     var errorHandler: ((_ error: Error) -> Void)?
     
+    var tagUpdateHandler: (() -> Void)?
+    
     required init(configuration: Configurable.Configuration = [:]) {
         self.configuration = configuration
     }
@@ -42,64 +44,72 @@ class TopListCoordinator<Digest: RealmWordDigest>: ListQueryCoordinatorable {
     func setupQuery(for section: Int = 0) {
         if let bookId = configuration["bookId"] as? String {
             // Query digests are related to a specific book
-            RealmBookRepository().queryBy(id: bookId) { [weak self] (success, result) in
-                guard success, let result = result, let self = self else {
-                    return
-                }
-                
-                self.bookResult = result
-                if Digest.self == RealmSentence.self {
-                    self.results = result.sentences.sorted(byKeyPath: "updatedAt", ascending: false) as? Results<Digest>
-                } else if Digest.self == RealmParagraph.self {
-                    self.results = result.paragraphs.sorted(byKeyPath: "updatedAt", ascending: false) as? Results<Digest>
-                }
-                
-                if let token = self.results?.observe({ [weak self] (changes) in
-                    guard let self = self else { return }
-                    switch changes {
-                    case .initial:
-                        self.initialHandler?(self.results)
-                    case .update(_, let deletions, let insertions, let modifications):
-                        self.updateHandler?(
-                            deletions.map { IndexPath(row: $0, section: section) },
-                            insertions.map { IndexPath(row: $0, section: section) },
-                            modifications.map { IndexPath(row: $0, section: section) }
-                        )
-                    case .error(let e):
-                        self.errorHandler?(e)
-                    }
-                }) {
-                    self.realmNotificationTokens.append(token)
-                }
-            }
+            setupQueryForSpecificBook(bookId: bookId, updatingSection: section)
         } else {
             // query all digests
-            repository.queryAllSortingByUpdatedAtDesc { [weak self] (success, _results) in
-                guard success, let results = _results, let self = self else {
-                    return
+            setupQueryForAll(updatingSection: section)
+        }
+    }
+    
+    private func setupQueryForSpecificBook(bookId: String, updatingSection section: Int) {
+        RealmBookRepository().queryBy(id: bookId) { [weak self] (success, result) in
+            guard success, let result = result, let self = self else {
+                return
+            }
+            
+            self.bookResult = result
+            if Digest.self == RealmSentence.self {
+                self.results = result.sentences.sorted(byKeyPath: "updatedAt", ascending: false) as? Results<Digest>
+            } else if Digest.self == RealmParagraph.self {
+                self.results = result.paragraphs.sorted(byKeyPath: "updatedAt", ascending: false) as? Results<Digest>
+            }
+            
+            if let token = self.results?.observe({ [weak self] (changes) in
+                guard let self = self else { return }
+                switch changes {
+                case .initial:
+                    self.initialHandler?(self.results)
+                case .update(_, let deletions, let insertions, let modifications):
+                    self.updateHandler?(
+                        deletions.map { IndexPath(row: $0, section: section) },
+                        insertions.map { IndexPath(row: $0, section: section) },
+                        modifications.map { IndexPath(row: $0, section: section) }
+                    )
+                case .error(let e):
+                    self.errorHandler?(e)
                 }
-                
-                // link to results
-                self.results = results
-                
-                // setup notification
-                if let token = self.results?.observe({ [weak self] (changes) in
-                    guard let self = self else { return }
-                    switch changes {
-                    case .initial:
-                        self.initialHandler?(self.results)
-                    case .update(_, let deletions, let insertions, let modifications):
-                        self.updateHandler?(
-                            deletions.map { IndexPath(row: $0, section: section) },
-                            insertions.map { IndexPath(row: $0, section: section) },
-                            modifications.map { IndexPath(row: $0, section: section) }
-                        )
-                    case .error(let e):
-                        self.errorHandler?(e)
-                    }
-                }) {
-                    self.realmNotificationTokens.append(token)
+            }) {
+                self.realmNotificationTokens.append(token)
+            }
+        }
+    }
+    
+    private func setupQueryForAll(updatingSection section: Int) {
+        repository.queryAllSortingByUpdatedAtDesc { [weak self] (success, _results) in
+            guard success, let results = _results, let self = self else {
+                return
+            }
+            
+            // link to results
+            self.results = results
+            
+            // setup notification
+            if let token = self.results?.observe({ [weak self] (changes) in
+                guard let self = self else { return }
+                switch changes {
+                case .initial:
+                    self.initialHandler?(self.results)
+                case .update(_, let deletions, let insertions, let modifications):
+                    self.updateHandler?(
+                        deletions.map { IndexPath(row: $0, section: section) },
+                        insertions.map { IndexPath(row: $0, section: section) },
+                        modifications.map { IndexPath(row: $0, section: section) }
+                    )
+                case .error(let e):
+                    self.errorHandler?(e)
                 }
+            }) {
+                self.realmNotificationTokens.append(token)
             }
         }
     }
