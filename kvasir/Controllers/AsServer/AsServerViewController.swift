745b5a20f5034b5a4738cc52e504df5a57d6b76a
Monsoir <monwingyeung@gmail.com>
2019-06-01 21:01:43 +0800

    update: 详细访问步骤

--- kvasir/Controllers/AsServer/AsServerViewController.swift
+++ kvasir/Controllers/AsServer/AsServerViewController.swift
@@ -48,7 +48,13 @@ class AsServerViewController: UIViewController, Configurable {
             MainQueue.async {
                 if success {
                     if let url = url {
-                        self.lbPrompt.text = "请在浏览器上访问：\n\(url.absoluteString)"
+                        self.lbPrompt.text = """
+                        开启服务器成功，访问步骤：
+                        
+                        1. 电脑或其他要访问资源的设备（「访问设备」），与手机连接同一个网络\n
+                        2. 在「访问设备」的浏览器上，输入以下地址进行访问\n
+                        \(url.absoluteString)
+                        """
                         return
                     }
                 }
